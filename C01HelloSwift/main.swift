9b00dd19b07f9bef67cc0e60aa66de349bad285f
20161104582 <a20161104582@scy.local>
2018-09-07 17:23:02 +0800

    Swift-hello world

---
+++ C01HelloSwift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  C01HelloSwift
+//
+//  Created by a20161104582 on 2018/9/7.
+//  Copyright © 2018年 a20161104582. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, swift!")
+
+
